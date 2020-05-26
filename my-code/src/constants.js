2349e189107f15b9ec168104ed356070296b1848
costa86 <costa86@zoho.com>
2020-05-26 12:07:39 +0100

    initial

--- my-code/src/constants.js
+++ my-code/src/constants.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 
-export const API_KEY = process.env.REACT_APP_KEY;
+//export const API_KEY = process.env.REACT_APP_KEY;
+export const API_KEY = "798e7a67";
 export const APP_NAME = "MOVIE SEARCH!";
