d70a661fc8f39f064ea770da072d4d4c3851f11f
costa86 <costa86@zoho.com>
2020-05-26 12:10:12 +0100

    Updates

---
+++ precache-manifest.83089eaaed19d9fca10939e71f52405a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0ba7208448b84f54102e79fa839bf0",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "d6bf6fabd38259a1aaa2",
+    "url": "/react-api/static/css/main.990fd3dd.chunk.css"
+  },
+  {
+    "revision": "1af2976a0db63e574bf0",
+    "url": "/react-api/static/js/2.5408905c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-api/static/js/2.5408905c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bf6fabd38259a1aaa2",
+    "url": "/react-api/static/js/main.c5171280.chunk.js"
+  },
+  {
+    "revision": "516e203d07580cec5256",
+    "url": "/react-api/static/js/runtime-main.bf60053c.js"
+  }
+]);
